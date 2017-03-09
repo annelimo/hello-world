8c7af79ed8a13e2ef850374602534f5d66da0124
annelimo <annshi.limz@gmail.com>
2017-03-09 13:22:30 +0300

    Add files via upload

---
+++ substractiontest.py
@@ -0,0 +1,13 @@
+import unittest
+from ANDELADAY import subtraction
+def subtraction (x,y):
+	return x-y
+
+class SimpleTest(unittest.TestCase):
+	def testsub
+	(self):
+	self.assertEqual(subtraction(3, 5), -2)
+
+
+if__name__=="__main__":
+unittest.main()
